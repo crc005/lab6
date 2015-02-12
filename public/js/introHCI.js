348ce7934c3a19ee28936972caf9e6a1b155ab12
Crystal Chung <crc005@ucsd.edu>
2015-02-12 13:38:05 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -22,11 +22,27 @@ function addProjectDetails(e) {
 	e.preventDefault();
 
 	// Get the div ID, e.g., "project3"
-	var projectID = $(this).closest('.project').attr('id');
+	var containingProject = $(this).closest(".project");
+	var projectID = containingProject.attr('id');
+	
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	console.log("/project/" + idNumber);
+
+	$.get("/project/" + idNumber, addProject);
+}
+
+function addProject(result) {
+	console.log(result);
+	var projectHTML = '<img src="' + result['image'] + '" class="detailsImage">' +
+	'<p>' + result['title'] +'</p>' +
+	'<p><small>' + result['date'] + '</small></p></a>' +
+	'<p>' + result['summary'] + '</p>';
+
+	var projectDetails = $("#project" + result['id'] + " .details");
+	projectDetails.html(projectHTML);
 }
 
 /*
@@ -35,4 +51,18 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", addColor);
 }
+
+function addColor(result) {
+	console.log(result);
+	
+	var colors = result.colors.hex;
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
+
